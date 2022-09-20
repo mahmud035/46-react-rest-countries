c72d7b3ad5890b77bd5e960a934ccf1ca7c9c555
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-20 22:28:46 +0600

    added animation to SingleCountry card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 import AllCountry from './Components/AllCountry/AllCountry';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="App">
       <AllCountry></AllCountry>
--- src/Components/SingleCountry/SingleCountry.js
+++ src/Components/SingleCountry/SingleCountry.js
@@ -14,7 +14,11 @@ const SingleCountry = ({ country }) => {
   } = country;
 
   return (
-    <Card className="h-100 border-0 shadow-sm ">
+    <Card
+      data-aos="fade-up"
+      data-aos-duration="1500"
+      className="h-100 border-0 shadow-sm "
+    >
       <Card.Img variant="top" src={png} />
       <Card.Body>
         <Card.Title>{common}</Card.Title>
